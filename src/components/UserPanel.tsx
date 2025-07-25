c77c6a9b3736d5026e6993599efbc9ba41656aa0
Codenames-Ren <renmizuki38@gmail.com>
2025-07-25 21:09:25 +0700

    menambah component user panel

---
+++ src/components/UserPanel.tsx
@@ -0,0 +1,11 @@
+export function UserPanel() {
+  return (
+    <div className="bg-black/50 px-4 py-2 rounded-lg flex items-center gap-4">
+      <div className="w-12 h-12 bg-white rounded-full" />
+      <div>
+        <div className="text-lg font-bold">Ren</div>
+        <div className="text-sm text-green-400">Lv. 20</div>
+      </div>
+    </div>
+  );
+}
